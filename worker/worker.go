083f5d3fb61b189a93912e59085ded76b25757f8
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2018-12-30 22:15:22 +0700

    cleanup worker

--- worker/worker.go
+++ worker/worker.go
@@ -29,39 +29,21 @@ func hash(s string) uint32 {
 }
 
 type partition struct {
-	worker_id   string
-	worker_host string
-	state       int // OFF, NORMAL, BLOCKED,
-
+	worker_id           string
+	worker_host         string
+	state               int // OFF, NORMAL, BLOCKED,
 	prepare_worker_id   string
 	prepare_worker_host string
 }
 
 type Worker struct {
 	*sync.Mutex
-	id     string // worker id
-	config *pb.Configuration
-
+	id         string
 	partitions []partition
-
-	conn map[string]*grpc.ClientConn
-
-	version        string
-	handlerOutType map[string]reflect.Type // key is method name (not fullmethod), value is type (not pointer) of return value
-	cluster        string
-	term           int
-	coor           pb.CoordinatorClient
-}
-
-// findConfig lookups worker configuration by worker ID in global configuration
-// return nil if not found
-func findConfig(conf *pb.Configuration, id string) *pb.WorkerConfiguration {
-	for _, w := range conf.GetWorkers() {
-		if w.GetId() == id {
-			return w
-		}
-	}
-	return nil
+	version    string
+	cluster    string
+	term       int
+	coor       pb.CoordinatorClient
 }
 
 func dialGrpc(service string) (*grpc.ClientConn, error) {
@@ -71,10 +53,6 @@ func dialGrpc(service string) (*grpc.ClientConn, error) {
 	opts = append(opts, grpc.WithBlock())
 	// However, we're still setting a timeout so that if the server takes too long, we still give up
 	opts = append(opts, grpc.WithTimeout(10*time.Second))
-	//res, err := naming.NewDNSResolverWithFreq(1 * time.Second)
-	//if err != nil {
-	//	return nil, err
-	//}
 	opts = append(opts, grpc.WithBalancerName(roundrobin.Name))
 	//opts = append(opts, grpc.WithBalancer(grpc.RoundRobin(res)))
 	return grpc.Dial(service, opts...)
@@ -101,7 +79,6 @@ func (me *Worker) fetchConfig() {
 	}
 
 	me.Lock()
-	me.config = conf
 	me.term = int(conf.GetTerm())
 	me.partitions = make([]partition, conf.GetTotalPartitions())
 	for _, w := range conf.GetWorkers() {
@@ -113,17 +90,16 @@ func (me *Worker) fetchConfig() {
 			me.partitions[p].state = NORMAL
 		}
 	}
+
 	me.Unlock()
 }
 
-func NewWorker(cluster, id, coordinator string) *Worker {
+func NewWorker(server *grpc.Server, cluster, id, coordinator string) *Worker {
 	me := &Worker{
-		Mutex:          &sync.Mutex{},
-		version:        "1.0.0",
-		cluster:        cluster,
-		conn:           make(map[string]*grpc.ClientConn),
-		handlerOutType: make(map[string]reflect.Type),
-		term:           0,
+		Mutex:   &sync.Mutex{},
+		version: "1.0.0",
+		cluster: cluster,
+		term:    0,
 	}
 
 	cconn, err := dialGrpc(coordinator)
@@ -132,6 +108,8 @@ func NewWorker(cluster, id, coordinator string) *Worker {
 	}
 	me.coor = pb.NewCoordinatorClient(cconn)
 	me.fetchConfig()
+
+	pb.RegisterWorkerServer(server, me)
 	return me
 }
 
@@ -150,19 +128,14 @@ func (me *Worker) validateRequest(version, cluster string, term int) error {
 	return nil
 }
 
-func (me *Worker) GetConfig(cluster, version string, term int) (*pb.Configuration, error) {
-	err := me.validateRequest(version, cluster, term)
-	if err != nil {
-		return nil, err
-	}
-
-	return me.config, nil
+func (me *Worker) GetConfig(ctx context.Context, em *pb.Empty) (*pb.Configuration, error) {
+	return me.coor.GetConfig(ctx, em)
 }
 
-func (me *Worker) Prepare(conf *pb.Configuration) error {
+func (me *Worker) Request(ctx context.Context, conf *pb.Configuration) (*pb.Empty, error) {
 	err := me.validateRequest(conf.GetVersion(), conf.GetCluster(), int(conf.GetTerm()))
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	me.Lock()
@@ -179,7 +152,7 @@ func (me *Worker) Prepare(conf *pb.Configuration) error {
 	me.Unlock()
 
 	go me.fetchConfig()
-	return nil
+	return nil, nil
 }
 
 func analysis(server interface{}) map[string]reflect.Type {
@@ -206,21 +179,12 @@ func analysis(server interface{}) map[string]reflect.Type {
 	return m
 }
 
-func (me *Worker) forward(ctx context.Context, host string, in interface{}, serverinfo *grpc.UnaryServerInfo) (interface{}, error) {
+// key is method name (not fullmethod), value is type (not pointer) of return value
+func (me *Worker) forward(cc *grpc.ClientConn, outType map[string]reflect.Type, ctx context.Context, in interface{}, serverinfo *grpc.UnaryServerInfo) (interface{}, error) {
 	md, _ := metadata.FromIncomingContext(ctx)
 	outctx := metadata.NewOutgoingContext(context.Background(), md)
 
-	cc := me.conn[host]
-	if cc == nil {
-		var err error
-		cc, err = dialGrpc(host)
-		if err != nil {
-			return nil, err
-		}
-		me.conn[host] = cc
-	}
-
-	out := reflect.New(me.handlerOutType[serverinfo.FullMethod]).Interface()
+	out := reflect.New(outType[serverinfo.FullMethod]).Interface()
 
 	var header, trailer metadata.MD
 	err := cc.Invoke(outctx, serverinfo.FullMethod, in, out, grpc.Header(&header), grpc.Trailer(&trailer))
@@ -230,30 +194,45 @@ func (me *Worker) forward(ctx context.Context, host string, in interface{}, serv
 	return out, err
 }
 
-func (me *Worker) Intercept(ctx context.Context, in interface{}, sinfo *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (out interface{}, err error) {
-	md, _ := metadata.FromIncomingContext(ctx)
-	pkey := strings.Join(md[PartitionKey], "")
+type interceptor func(ctx context.Context, in interface{}, sinfo *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (out interface{}, err error)
 
-	if pkey == "" {
-		return handler(ctx, in)
-	}
+func (me *Worker) CreateIntercept(mgr interface{}) interceptor {
+	outTypeM := analysis(mgr)
+	conn := &sync.Map{}
+	return func(ctx context.Context, in interface{}, sinfo *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (out interface{}, err error) {
+		md, _ := metadata.FromIncomingContext(ctx)
+		pkey := strings.Join(md[PartitionKey], "")
 
-	parindex := hash(pkey) % 1000
-	me.Lock()
-	par := me.partitions[parindex]
-	me.Unlock()
+		if pkey == "" {
+			return handler(ctx, in)
+		}
 
-	// block
-	for par.state == BLOCKED {
-		time.Sleep(5 * time.Second)
+		parindex := hash(pkey) % 1000
 		me.Lock()
-		par = me.partitions[parindex]
+		par := me.partitions[parindex]
 		me.Unlock()
-	}
 
-	if par.worker_id == me.id { // correct parittion
-		return handler(ctx, in)
-	}
+		// block
+		for par.state == BLOCKED {
+			time.Sleep(5 * time.Second)
+			me.Lock()
+			par = me.partitions[parindex]
+			me.Unlock()
+		}
 
-	return me.forward(ctx, par.worker_host, in, sinfo)
+		if par.worker_id == me.id { // correct parittion
+			return handler(ctx, in)
+		}
+
+		cci, ok := conn.Load(par.worker_host)
+		if !ok {
+			var err error
+			cci, err = dialGrpc(par.worker_host)
+			if err != nil {
+				return nil, err
+			}
+			conn.Store(par.worker_host, cci)
+		}
+		return me.forward(cci.(*grpc.ClientConn), outTypeM, ctx, in, sinfo)
+	}
 }
