0a46db333b6491b371f8f375a1b6795ec04e707f
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2019-01-02 20:29:16 +0700

    add comment

--- client/client.go
+++ client/client.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	pb "github.com/subiz/header/partitioner"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/balancer/roundrobin"
 	"google.golang.org/grpc/metadata"
 	"hash/fnv"
 	"strings"
@@ -14,15 +13,15 @@ import (
 )
 
 const (
+	// GRPC Metadata key to store partition key attached to each GRPC request
+	// the key will be hashed to find the correct worker that handle the request
 	PartitionKey = "partitionkey"
 )
 
-func hash(s string) uint32 {
-	h := fnv.New32a()
-	h.Write([]byte(s))
-	return h.Sum32()
-}
-
+// Client is used to partition request through workers in cluster
+// user uses this struct by create an GRPC interceptor, the interceptor will
+// trigger right before the request is sent, redirecting the request to assigned
+// worker
 type Client struct {
 	*sync.RWMutex
 	conn       map[string]*grpc.ClientConn
@@ -35,26 +34,23 @@ func dialGrpc(service string) (*grpc.ClientConn, error) {
 	// Enabling WithBlock tells the client to not give up trying to find a server
 	opts = append(opts, grpc.WithBlock())
 	// However, we're still setting a timeout so that if the server takes too long, we still give up
-	opts = append(opts, grpc.WithTimeout(10*time.Second))
-	//res, err := naming.NewDNSResolverWithFreq(1 * time.Second)
-	//if err != nil {
-	//	return nil, err
-	//}
-	opts = append(opts, grpc.WithBalancerName(roundrobin.Name))
-	//opts = append(opts, grpc.WithBalancer(grpc.RoundRobin(res)))
+	opts = append(opts, grpc.WithTimeout(2*time.Second))
 	return grpc.Dial(service, opts...)
 }
 
-func NewClient(service string) *Client {
-	// connect to grpc cluster
+// NewInterceptor create new GRPC interceptor to partition request
+// service is one of workers host, e.g: web-0.web:8080. This host will be used
+// to discover all workers in cluster.
+func NewInterceptor(service string) grpc.DialOption {
+	me := &Client{RWMutex: &sync.RWMutex{}}
+	me.conn = make(map[string]*grpc.ClientConn)
 
 	pconn, err := dialGrpc(service)
 	if err != nil {
 		panic(err)
 	}
-	// parittioner client
+
 	pclient := pb.NewWorkerClient(pconn)
-	me := &Client{RWMutex: &sync.RWMutex{}}
 
 	pars, err := fetchPartitions(pclient)
 	if err != nil {
@@ -65,15 +61,24 @@ func NewClient(service string) *Client {
 
 	// update cluster state every 30s
 	go me.fetchLoop(pclient)
-	return me
+
+	return grpc.WithUnaryInterceptor(me.clientInterceptor)
 }
 
+// fetchPartitions calls worker api to return the latest partition map
+// partition map is an array tells which worker will handle which partition
+// partition number is ordinal index of array, each element contains worker
+// host
+// eg: ["worker-0:8081", "worker-1:8081", "worker-0:8081"] has 3 partitions:
+// {0, 1, 2}, partition 0 and 2 are handled by worker 0 at host worker-0:8081
+// partition 1 is handled by worker 1 at host worker-1:8081
 func fetchPartitions(pclient pb.WorkerClient) ([]string, error) {
 	conf, err := pclient.GetConfig(context.Background(), &pb.Cluster{})
 	if err != nil {
 		return nil, err
 	}
 
+	// convert configuration fetched from worker to partition map
 	partitions := make([]string, conf.GetTotalPartitions())
 	for workerid, pars := range conf.GetPartitions() {
 		for _, parNum := range pars.GetPartitions() {
@@ -83,11 +88,57 @@ func fetchPartitions(pclient pb.WorkerClient) ([]string, error) {
 	return partitions, nil
 }
 
+// clientInterceptor is an GRPC client interceptor, it redirects the request to
+// the correct worker host
+// We find worker host by hashing partition key to a partition number then
+// look it up in the current partition map
+// worker host might not be the correct one since partition map can be
+// unsynchronized (by network lantences or network partitioned). When it
+// happend, request will be redirected one or more times in worker side till
+// it arrive the correct host. This may look ineffiecient but we don't have to
+// guarrantee consistent at client-side, make client-side code so much simpler
+// when the  network is stable and no worker member changes (most of the time),
+// partition map is in-sync, clients are now sending requests directly to the
+// correct host without making any additional redirection.
+func (me *Client) clientInterceptor(ctx context.Context, method string, in interface{}, out interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	md, _ := metadata.FromOutgoingContext(ctx)
+	pkey := strings.Join(md[PartitionKey], "")
+
+	if pkey == "" {
+		return invoker(ctx, method, in, out, cc, opts...)
+	}
+
+	// hashing key to find the partition number
+	h := fnv.New32a()
+	h.Write([]byte(pkey))
+	par := h.Sum32()  % 1000
+
+	host := me.partitions[par]
+	me.Lock()
+	cc = me.conn[host]
+	if cc == nil {
+		var err error
+		cc, err = func() (*grpc.ClientConn, error) {
+			defer func() { recover() }()
+			return dialGrpc(host) // TODO: this could cause bottle neck if user send
+			// many requests to a not-responding host
+		}()
+		if err != nil {
+			me.Unlock()
+			return err
+		}
+		me.conn[host] = cc
+		me.Unlock()
+	}
+	return cc.Invoke(ctx, method, in, out, opts...)
+}
+
+// fetchLoop synchronizes partition in every 30 sec
 func (me *Client) fetchLoop(pclient pb.WorkerClient) {
 	for {
 		var pars []string
 		var err error
-		func() {
+		func() { // wrap in a function to prevent panicing
 			defer func() { recover() }()
 			pars, err = fetchPartitions(pclient)
 			if err != nil {
@@ -104,37 +155,4 @@ func (me *Client) fetchLoop(pclient pb.WorkerClient) {
 		me.Unlock()
 		time.Sleep(30 * time.Second)
 	}
-}
-
-// forward to the right host
-func (me *Client) clientInterceptor(ctx context.Context, method string, in interface{}, out interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
-	md, _ := metadata.FromOutgoingContext(ctx)
-	pkey := strings.Join(md[PartitionKey], "")
-
-	if pkey == "" {
-		return invoker(ctx, method, in, out, cc, opts...)
-	}
-
-	par := hash(pkey) % 1000
-	host := me.partitions[par]
-	cc = me.conn[host]
-	if cc == nil {
-		var err error
-		me.Lock()
-		cc, err = func() (*grpc.ClientConn, error) {
-			defer func() { recover() }()
-			return dialGrpc(host)
-		}()
-		if err != nil {
-			me.Unlock()
-			return err
-		}
-		me.conn[host] = cc
-	}
-
-	return cc.Invoke(ctx, method, in, out, opts...)
-}
-
-func (me *Client) WithInterceptor() grpc.DialOption {
-	return grpc.WithUnaryInterceptor(me.clientInterceptor)
 }
