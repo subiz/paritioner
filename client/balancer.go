88e1c2592ea650a782a6686ba4112b555e44afb9
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2019-01-06 12:30:44 +0700

    fix balancer

--- client/balancer.go
+++ client/balancer.go
@@ -1,69 +1,61 @@
-// Package client defines a roundrobin balancer. Roundrobin balancer is
-// installed as one of the default balancers in gRPC, users don't need to
-// explicitly install this balancer.
-
 package client
 
 import (
+	"context"
 	"fmt"
-	"strings"
-	"sync"
-
-	"golang.org/x/net/context"
+	"github.com/subiz/errors"
+	pb "github.com/subiz/header/partitioner"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/balancer"
+	"google.golang.org/grpc/connectivity"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/resolver"
+	"math/rand"
+	"strings"
+	"sync"
+	"time"
 )
 
 // Name is the name of partitioner balancer.
 const Name = "partitioner"
 
 func init() {
-	balancer.Register(&parBuilder{name: Name, pickerBuilder: &pPickerBuilder{}})
+	rand.Seed(time.Now().UnixNano())
+	balancer.Register(&parBuilder{name: Name})
 }
 
-// partitioner picker builder
-type pPickerBuilder struct{}
-
-func (*pPickerBuilder) Build(pars []string, readySCs map[resolver.Address]balancer.SubConn) balancer.Picker {
-	fmt.Printf("partitionerPicker: newPicker called with readySCs: %v\n", readySCs)
-	var scs []balancer.SubConn
-
-	subConnHost := make(map[string]balancer.SubConn)
-	for a, sc := range readySCs {
-		scs = append(scs, sc)
-		subConnHost[a.Addr] = sc
-	}
-	return &parPicker{subConns: scs, partitions: pars, subConnHost: subConnHost}
-}
+func NewParPicker() *parPicker { return &parPicker{Mutex: &sync.Mutex{}} }
 
 type parPicker struct {
 	*sync.Mutex
-	// subConns is the snapshot of the roundrobin balancer when this picker was
-	// created. The slice is immutable. Each Get() will do a round robin
-	// selection from it and return the selected SubConn.
-	subConns []balancer.SubConn
+	partitions []string
+	subConns   map[string]balancer.SubConn
+}
 
-	next int
+func (me *parPicker) Update(pars []string, subConns map[string]balancer.SubConn) {
+	me.Lock()
+	defer me.Unlock()
+	fmt.Printf("partitionerPicker: newPicker called with readySCs: %v\n", pars)
 
-	partitions  []string
-	subConnHost map[string]balancer.SubConn
+	// copy subConns
+	me.subConns = make(map[string]balancer.SubConn)
+	for host, sc := range subConns {
+		me.subConns[host] = sc
+	}
+
+	me.partitions = pars
 }
 
 func (me *parPicker) Pick(ctx context.Context, opts balancer.PickOptions) (balancer.SubConn, func(balancer.DoneInfo), error) {
-	if len(p.subConns) <= 0 {
+	if len(me.subConns) <= 0 {
 		return nil, nil, balancer.ErrNoSubConnAvailable
 	}
 
 	md, _ := metadata.FromOutgoingContext(ctx)
 	pkey := strings.Join(md[PartitionKey], "")
 
-	if pkey == "" { // round robin
-		me.Lock()
-		sc := me.subConns[me.next]
-		me.next = (me.next + 1) % len(me.subConns)
-		me.Unlock()
-		return sc, nil, nil
+	if pkey == "" { // random host
+		pkey = fmt.Sprintf("%d", rand.Int())
 	}
 
 	// hashing key to find the partition number
@@ -72,11 +64,166 @@ func (me *parPicker) Pick(ctx context.Context, opts balancer.PickOptions) (balan
 	ghash.Reset()
 
 	me.Lock()
-	host := me.partitions[par]
-	sc := me.subConnHost[host]
+	sc := me.subConns[me.partitions[par]]
 	me.Unlock()
 	if sc == nil {
 		return nil, nil, balancer.ErrNoSubConnAvailable
 	}
 	return sc, nil, nil
+}
+
+type parBuilder struct {
+	name string
+}
+
+func (me *parBuilder) Build(cc balancer.ClientConn, opt balancer.BuildOptions) balancer.Balancer {
+	return &parBalancer{
+		Mutex:    &sync.Mutex{},
+		cc:       cc,
+		subConns: make(map[string]balancer.SubConn),
+		picker:   NewParPicker(),
+	}
+}
+
+func (me *parBuilder) Name() string { return me.name }
+
+// partitioner balancer
+type parBalancer struct {
+	*sync.Mutex
+	cc balancer.ClientConn
+
+	subConns map[string]balancer.SubConn
+
+	picker     balancer.Picker
+	conn       map[string]*grpc.ClientConn
+	partitions []string
+
+	// tells that fetchLoop is running or not
+	fetching bool
+}
+
+// fetchLoop synchronizes partition in every 30 sec
+func (me *parBalancer) fetchLoop(service string) {
+	if me.fetching {
+		return
+	}
+
+	me.fetching = true
+	pconn, err := dialGrpc(service)
+	if err != nil {
+		panic(err)
+	}
+
+	pclient := pb.NewWorkerClient(pconn)
+	// update cluster state every 30s
+	for {
+		pars, err := fetchPartitions(pclient)
+		if err != nil {
+			fmt.Printf("ERR#FS94GPOFD fetching partition: %v\n", err)
+		}
+
+		if len(pars) == 0 {
+			continue
+		}
+
+		hostM := make(map[string]bool, 0)
+		for _, host := range pars {
+			hostM[host] = true
+		}
+
+		hosts := make([]string, 0)
+		for host := range hostM {
+			hosts = append(hosts, host)
+		}
+
+		addrsSet := make(map[string]struct{})
+		for _, host := range hosts {
+			addrsSet[host] = struct{}{}
+			if _, ok := me.subConns[host]; !ok {
+				sc, err := me.cc.NewSubConn([]resolver.Address{{
+					Addr: host,
+					Type: resolver.Backend,
+				}}, balancer.NewSubConnOptions{HealthCheckEnabled: false})
+				if err != nil {
+					fmt.Printf("ERR HSJKH849FDSF %v", err)
+					continue
+				}
+				me.subConns[host] = sc
+				sc.Connect()
+			}
+		}
+
+		for h, sc := range me.subConns {
+			// host h was removed by resolver.
+			if _, ok := addrsSet[h]; !ok {
+				me.cc.RemoveSubConn(sc)
+				delete(me.subConns, h)
+				// Keep the state of this sc in b.scStates until sc's state becomes Shutdown.
+				// The entry will be deleted in HandleSubConnStateChange.
+			}
+		}
+
+		me.Lock()
+		me.partitions = pars
+		me.Unlock()
+		time.Sleep(30 * time.Second)
+	}
+}
+
+// ignore since we dont rely in resolver addrs
+func (me *parBalancer) HandleResolvedAddrs(addrs []resolver.Address, err error) {
+	if err != nil {
+		fmt.Printf("ERR base.baseBalancer: HandleResolvedAddrs called with error %v", err)
+		return
+	}
+
+	a := addrs[0]
+	me.fetchLoop(a.Addr)
+}
+
+// also ignore since we have our own implementation
+func (me *parBalancer) HandleSubConnStateChange(sc balancer.SubConn, s connectivity.State) {
+	fmt.Printf("partitioner.parBalancer: handle SubConn state change: %p, %v\n", sc, s)
+	if s == connectivity.Idle {
+		sc.Connect()
+	}
+	me.cc.UpdateBalancerState(connectivity.Ready, me.picker)
+}
+
+// Close is a nop because base balancer doesn't have internal state to clean up,
+// and it doesn't need to call RemoveSubConn for the SubConns.
+func (_ *parBalancer) Close() {}
+
+// fetchPartitions calls worker api to return the latest partition map
+// partition map is an array tells which worker will handle which partition
+// partition number is ordinal index of array, each element contains worker
+// host
+// eg: ["worker-0:8081", "worker-1:8081", "worker-0:8081"] has 3 partitions:
+// {0, 1, 2}, partition 0 and 2 are handled by worker 0 at host worker-0:8081
+// partition 1 is handled by worker 1 at host worker-1:8081
+func fetchPartitions(pclient pb.WorkerClient) (pars []string, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			var ok bool
+			if err, ok = r.(error); ok {
+				err = errors.Wrap(err, 500, errors.E_unknown)
+				return
+			}
+			err = errors.New(500, errors.E_unknown, r)
+		}
+	}()
+
+	conf, err := pclient.GetConfig(context.Background(), &pb.Cluster{})
+	if err != nil {
+		return nil, err
+	}
+
+	// convert configuration fetched from worker to partition map
+	partitions := make([]string, conf.GetTotalPartitions())
+	for workerid, pars := range conf.GetPartitions() {
+		for _, parNum := range pars.GetPartitions() {
+			partitions[parNum] = conf.GetHosts()[workerid]
+		}
+	}
+	return partitions, nil
 }
