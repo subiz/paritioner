e9ddf6ac11bbc95f277659dc5231c41272829b00
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2019-02-11 23:40:47 +0700

    add header

---
+++ header/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# snap install protobuf --classic
+#GIT_TAG="v1.2.0" # change as needed
+# go get -d -u github.com/golang/protobuf/protoc-gen-go
+# git -C "$(go env GOPATH)"/src/github.com/golang/protobuf checkout $GIT_TAG
+# go install github.com/golang/protobuf/protoc-gen-go
+/snap/bin/protoc --go_out=. *.proto
