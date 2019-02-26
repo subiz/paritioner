862e8a01ed68ef2889f973b97ba46159f6ad2b91
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2019-02-27 00:06:32 +0700

    add coordinator db mock comments

--- coordinator/db_mock.go
+++ coordinator/db_mock.go
@@ -2,22 +2,35 @@ package main
 
 import (
 	pb "github.com/subiz/partitioner/header"
+	"sync"
 )
 
+// DBMock is a mock of DB struct for testing purpose only
 type DBMock struct {
+	*sync.Mutex
 	store map[string]pb.Configuration
 }
 
+// NewDBMock creates a new DBMock object
 func NewDBMock() *DBMock {
-	return &DBMock{store: make(map[string]pb.Configuration)}
+	return &DBMock{
+		Mutex: &sync.Mutex{},
+		store: make(map[string]pb.Configuration),
+	}
 }
 
+// Load lookups configuration for cluster in the database
 func (me *DBMock) Load(cluster string) (*pb.Configuration, error) {
+	me.Lock()
+	defer me.Unlock()
 	conf := me.store[cluster]
 	return &conf, nil
 }
 
+// Store persists configuration to the database
 func (me *DBMock) Store(conf *pb.Configuration) error {
+	me.Lock()
+	defer me.Unlock()
 	me.store[conf.Cluster] = *conf
 	return nil
 }
